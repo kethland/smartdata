9d212772e7df5eb0eed9b5d134cd4fb843890078
kethland <keth45406@gmail.com>
2019-11-14 23:01:56 -0500

    Added package setup and unit test module

---
+++ org.smartdata.meetup/src/module-info.java
@@ -0,0 +1,4 @@
+module org.smartdata.meetup {
+	requires junit;
+	requires org.junit.jupiter.api;
+}
---
+++ org.smartdata.meetup/src/test/BabysitterTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BabysitterTest {
+
+	@Test
+	void test() {
+		assertEquals("type", "type");
+	}
+
+}
